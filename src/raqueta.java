5b23cf08da1d7299b3c827a15d6f51220fa2688a
CarlosSapien03 <112510359+CarlosSapien03@users.noreply.github.com>
2022-10-22 12:59:37 -0700

    Ping Pong, tarea DS3.

---
+++ src/raqueta.java
@@ -0,0 +1,9 @@
+public class raqueta {
+    public boolean check1(int bxmin, int bymin) {
+        return false;
+    }
+
+    public boolean check2(int bxmax, int bymin) {
+        return false;
+    }
+}
